17bc2a56ca7be1b1c157e96fb1fdc4309edc875a
ha3al <ha3alkaya@gmail.com>
2018-01-29 14:56:27 +0300

    login configuration

--- election/urls.py
+++ election/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home),
     url(r'^deneme/', views.deneme),
+    url(r'^login/$', views.login_view, name='login'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- election/views.py
+++ election/views.py
@@ -1,5 +1,6 @@
 from django.http import *
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 def home(request):
     if request.method == 'POST':
@@ -7,9 +8,25 @@ def home(request):
         email = request.POST['email']
         return render(request, 'index.html', {"ad": ad, "email": email})
     else:
+        print(request.user.email)
         return render(request, "index.html")
 
 
 
 def deneme(request):
     return render(request, 'deneme.html', locals())
+
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username= email, password= password)
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            return render(request, "login_view.html")
+
+    return render(request, "login_view.html")
+
+
