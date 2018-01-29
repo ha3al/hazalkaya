95aeb4293fa409afe7291b0575f3626656959f22
ha3al <ha3alkaya@gmail.com>
2018-01-29 11:15:16 +0300

    GET/POST

--- election/views.py
+++ election/views.py
@@ -2,7 +2,14 @@ from django.http import *
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html', locals())
+    if request.method == 'POST':
+        ad = request.POST['ad']
+        email = request.POST['email']
+        return render(request, 'index.html', {"ad": ad, "email": email})
+    else:
+        return render(request, "index.html")
+
+
 
 def deneme(request):
     return render(request, 'deneme.html', locals())
